a50a5fdb2589180c920e625a1faabaaa7f10c079
thanh@ubuntu <thanhdoanhuujp@gmail.com>
2018-08-22 21:01:02 +0900

    edit pc_client.py

--- pc_client.py
+++ pc_client.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python
- 
-# RasPi.vn
-# import context  # Ensures paho is in PYTHONPATH
-import paho.mqtt.publish as publish
-import paho.mqtt.subscribe as subscribe
+import time
+import paho.mqtt.client as paho
 import socket
 import os
 import json
+
 
 class SendClass(object):
     def __init__(self):
@@ -20,33 +17,43 @@ sendOject = SendClass()
 #brokerHost = "172.20.10.10" #Iphone
 brokerHost = "192.168.43.50" #Leo
 
-topics = "raspivn/demo/led"
+topic = "raspivn/demo/led"
+topicReceive = "rapivn/status"
 
-computerName = socket.gethostname()
-computerIpAdress = socket.gethostbyname(socket.getfqdn())
-print(computerName)
-print(computerIpAdress)
+#define callback
+def on_message(client, userdata, message):
+    print("received: ",str(message.payload.decode("utf-8")))
+
+client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+######Bind function to callback
+client.on_message=on_message
+#####
+print("connecting to broker ",brokerHost)
+client.connect(brokerHost)#connect
+client.loop_start() #start loop to process received messages
+print("subscribing ")
+client.subscribe(topicReceive)#subscribe
+
 input = raw_input("type [on] to turn on LED or type [off] to turn off LED, type [exit] to exit:\n")
 print("Ban chon " + input + "!")
-
 run = True
 while(run):
 	if(input == 'off'):
 		sendOject.message = 0
-	# publish.single(topics, "0", hostname = brokerHost) 
 	elif(input == 'on'):
 		sendOject.message = 1
-	# publish.single(topics, "1", hostname = brokerHost) 
 	elif(input == 'exit'):
 		break
+	else:
+		sendOject.message = 2
 
 	sendMessage = json.dumps(sendOject.__dict__) 
-	print(sendMessage)
-	publish.single(topics, sendMessage, hostname = brokerHost)
+	# print("publishing: ", sendMessage)
+	
+	client.publish(topic,sendMessage)#publish
 
 	input = raw_input("")
-	print("Ban chon " + input + "!")
+	print("Your choose: " + input + "!")
 
-print ("Chuong trinh vua thoat")
-
-
+client.disconnect() #disconnect
+client.loop_stop() #stop loop
