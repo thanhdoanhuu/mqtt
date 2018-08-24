b3b65f17c04571c513c895009f4bc9b0077d8684
Thanh <thanh.doanhuu@pasonatech.jp>
2018-08-24 11:42:25 +0900

    change ip address

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // host = '172.16.153.122';	// hostname or IP address
-//host = '172.21.33.25';	// hostname or IP address
+host = '172.21.33.25';	//Pi on mac
 // host = '192.168.43.50';	// hostname or IP address
-host = '10.0.2.15';
-// host = '172.16.153.110';	// hostname or IP address
+//host = '10.0.2.15';
+//host = '172.16.153.110';	// hostname or IP address
 port = 9001;
 topic = 'rapivn/status';		// topic to subscribe to
 useTLS = false;
