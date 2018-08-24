e6d20cc66f25b689c3a9d40dd06284323f09db87
Thanh <thanh.doanhuu@pasonatech.jp>
2018-08-24 11:47:46 +0900

    delete space

--- define.py
+++ define.py
@@ -9,7 +9,7 @@ class SendClass(object):
 #brokerHost = "172.21.33.24" #Rapian vituabox
 #brokerHost = "172.20.10.10" #Iphone
 #brokerHost = "192.168.43.50" #Leo
- brokerHost = "172.21.33.25" #raspberry mac
+brokerHost = "172.21.33.25" #raspberry mac
 
 topic = "raspivn/demo/led"
 topicReceive = "rapivn/status"
